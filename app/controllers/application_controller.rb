6b3729df945947dbb33d92db656ad610370e20a0
Jeongmin Byun <jmbyun91@gmail.com>
2018-05-31 13:55:15 +0900

    fix user create and edit glitch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user_with_token_and_cookie
+
+  private
+    def authenticate_user_with_token_and_cookie
+      authenticate_user!
+    end
 end
